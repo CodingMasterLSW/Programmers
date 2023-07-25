8003fcfeb335588f42068a8625af7bb5781c1a9c
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2023-07-25 16:32:51 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        int a = n*12000;
+        int b = (k*2000)-((n/10)*2000);
+        
+        
+        return a+b;
+    }
+}

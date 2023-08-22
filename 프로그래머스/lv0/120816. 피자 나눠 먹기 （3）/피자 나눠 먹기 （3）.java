339dad74c13c44527cc54c9496165747cf95bc6f
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2023-08-22 22:24:51 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int slice, int n) {
-        int result = 0;
-        if (n%slice == 0){
-            result = n/slice;
-        } else {
-            result = (n/slice)+1;
-        }
-        return result;
-    } 
+        return n%slice==0 ? n/slice : n/slice+1;
+        
+    }
 }

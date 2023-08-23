d61259774033be5617e96cb8e27dfaeeac9131c9
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2023-08-23 11:01:49 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,14 +1,8 @@
 class Solution {
     public String solution(String my_string) {
-        int n = my_string.length();
-        char[] arr = my_string.toCharArray();
-        char[] reversedArr = new char[n];
-
-        for (int i = 0; i < n; i++) {
-            reversedArr[i] = arr[n - 1 - i];
-        }
-
-        String result = new String(reversedArr);
-        return result;
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+        return sb.toString();
     }
 }

654ec6622a8c932c1c7f223908dc322a5a3746e4
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2023-08-23 10:54:56 +0900

    [level 0] Title: 배열 원소의 길이, Time: 2.08 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] solution(String[] strList) {
-        return Arrays.stream(strList).mapToInt(String::length).toArray();
-    }
+    public int[] solution(String[] strlist) {
+        return Arrays.stream(strlist).mapToInt(String::length).toArray();
+}
 }

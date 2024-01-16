552e4629560dd7f38654c0eee79d553b12c08d7c
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-01-16 16:30:08 +0900

    [Bronze II] Title: 분해합, Time: 244 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -3,31 +3,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        int changeInput = Integer.parseInt(input);
 
-        for (int i = 0; i < changeInput; i++) {
-            int finalResult = i + calculateResult(i);
-            if (finalResult == changeInput) {
-                System.out.println(i);
+        int result = 0;
+
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        for (int i = 0; i < input; i++) {
+            int number = i;
+            int calculateResult = 0;
+
+            while (number != 0) {
+                calculateResult += (number % 10);
+                number /= 10;
+            }
+
+            if (input == i + calculateResult) {
+                result = i;
                 break;
             }
-            if(i==changeInput-1){
-                System.out.println(0);
-            }
-        }
-    }
 
-    // i 들어왔을 때 계산 로직
-    private static int calculateResult(int k) {
-        int result = 0;
-        String changeK = String.valueOf(k);
-        for (char c : changeK.toCharArray()) {
-            result += Integer.parseInt(Character.toString(c));
         }
-        return result;
+        System.out.println(result);
 
     }
+
+
 }
-

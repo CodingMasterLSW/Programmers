f548e2b30c40597e9dbd9e34037007a8595818af
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-02-08 11:19:33 +0900

    [Silver II] Title: 최소 힙, Time: 356 ms, Memory: 26516 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static PriorityQueue<Integer> q = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (q.isEmpty()) {
+                    bw.write(String.valueOf(0));
+                    bw.newLine();
+                } else{
+                    bw.write(String.valueOf(q.poll()));
+                    bw.newLine();
+                }
+            } else {
+                q.add(x);
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+
+}

ca4502f5dba43202e79c034cf96be0c7444f96c7
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-07-04 00:27:19 +0900

    [Silver III] Title: N과 M (3), Time: 860 ms, Memory: 215496 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        int[] resources = new int[M];
+        backtracking(N, M, resources, 0);
+        System.out.println(sb.toString());
+
+    }
+    public static void backtracking(int n, int m, int[] resources, int depth){
+
+        if(M == depth){
+            for(int i=0; i<M; i++){
+                sb.append(resources[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=N; i++){
+            resources[depth] = i;
+            backtracking(n,m, resources, depth+1);
+        }
+    }
+}

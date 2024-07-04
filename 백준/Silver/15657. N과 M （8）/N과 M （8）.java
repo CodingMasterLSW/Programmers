2cf69e923bed47aea96fad5d13ce1e0537c1a57d
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-07-04 11:32:30 +0900

    [Silver III] Title: N과 M (8), Time: 292 ms, Memory: 23516 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        visited = new boolean[N];
+
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int[] resources = new int[M];
+
+        backtracking(N, M, visited, resources, 0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void backtracking(int n, int m, boolean[] visited, int[] resources, int depth,
+            int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(resources[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            resources[depth] = arr[i];
+            backtracking(n, m, visited, resources, depth + 1, i);
+        }
+
+
+    }
+}
+

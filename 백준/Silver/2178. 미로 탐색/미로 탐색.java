25476b3e4a9c24c810184a1220b297bc48aefe5c
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-07-26 02:23:40 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,92 +1,66 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static int n, m;
-    public static int[][] graph = new int[1000][1000];
-    public static int dx[] = {-1, 1, 0, 0};
-    public static int dy[] = {0, 0, -1, 1};
+    static int N;
+    static int M;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
 
-
-    public static int bfs(int x, int y) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(x, y));
-
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            x = node.getX();
-            y = node.getY();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    continue;
-                }
-                if (graph[nx][ny] == 0) { // 0이면 이동할 수 없음
-                    continue;
-                }
-                if (graph[nx][ny] == 1) {
-                    graph[nx][ny] = graph[x][y] + 1;
-                    q.offer(new Node(nx, ny));
-                }
-            }
-        }
-        return graph[n - 1][m - 1];
-
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st1.nextToken());
-        m = Integer.parseInt(st1.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        graph = new int[n][m];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i=0; i<n; i++){
-            String line = br.readLine();
-            for(int j=0; j<m; j++){
-                graph[i][j] = line.charAt(j)-'0';
+        graph = new int[N][M];
+        visited = new boolean[N][M];
+
+        for(int i=0; i<N; i++){
+            String s = br.readLine();
+            for(int j=0; j<M; j++){
+                graph[i][j] = s.charAt(j) - '0';
+            }
+        }
+        System.out.println(bfs(0,0));
+    }
+
+    public static int bfs(int x, int y){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x,y});
+        visited[x][y] = true;
+
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            for(int i=0; i<4; i++){
+                int ndx = currentX + dx[i];
+                int ndy = currentY + dy[i];
+
+                if (ndx < 0 || ndx >= N || ndy < 0 || ndy >= M) {
+                    continue;
+                }
+                if (visited[ndx][ndy] || graph[ndx][ndy] != 1){
+                    continue;
+                }
+
+                visited[ndx][ndy] = true;
+                q.offer(new int[]{ndx,ndy});
+
+                graph[ndx][ndy] = graph[currentX][currentY]+1;
+
+                if(ndx == N-1 && ndy == M-1){
+                    return graph[ndx][ndy];
+                }
 
             }
         }
-        graph[0][0] = 1;
-
-        int result = bfs(0, 0);
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
-
-    }
-
-
-}
-
-class Node {
-
-    private int x;
-    private int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
+        return -1;
     }
 }

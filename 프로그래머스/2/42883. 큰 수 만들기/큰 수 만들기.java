41eadcbaa78c0795b6ff72d1c1c3f534f9807411
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-09 15:33:11 +0900

    [level 2] Title: 큰 수 만들기, Time: 63.00 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,27 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         Stack<Character> stack = new Stack<>();
-        int len = number.length();
-        
-        for (int i=0; i<len; i++){
+        for(int i=0; i<number.length(); i++){
+            
             char c = number.charAt(i);
-            
-            while (!stack.isEmpty() && k > 0 && stack.peek() < c) {
+            while(!stack.isEmpty() && k>0 && stack.peek()<c){
                 stack.pop();
-                k--;  // 숫자를 하나 제거했으므로 k 감소
+                k--;
             }
-            stack.push(c);
             
+            stack.push(c);
         }
-        while (k > 0) {
+        
+        while(k>0){
             stack.pop();
             k--;
         }
         
-        StringBuilder answer = new StringBuilder();
-        for (char c : stack) {
-            answer.append(c);
+        StringBuilder sb = new StringBuilder();
+        for(char c : stack){
+            sb.append(c);
         }
-        return answer.toString();
+        return sb.toString();
     }
 }

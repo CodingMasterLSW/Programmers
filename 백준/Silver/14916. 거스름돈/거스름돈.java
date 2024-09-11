060549c33642e06fd699f2d35cd45b0d77f8feec
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-12 01:14:01 +0900

    [Silver V] Title: 거스름돈, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while (N > 0) {
+            if (N % 5 == 0) {
+                N -= 5;
+                count++;
+            } else {
+                N -= 2;
+                count++;
+            }
+        }
+        if (N < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(count);
+        }
+    }
+
+}

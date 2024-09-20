b9096883485eea7d48dec870a33599834423a600
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-20 17:53:31 +0900

    [Silver IV] Title: 수들의 합 2, Time: 124 ms, Memory: 15060 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,42 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int length = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int a[] = new int[n+1];
+        
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int sum=0;
+		int left=0;
+		int right=0;
+		int ans = 0;
 
-        List<Integer> lists = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < length; i++) {
-            lists.add(Integer.parseInt(st.nextToken()));
-        }
+		while(right <= n) {
+			if(sum >= s) {
+				sum -= a[left++];
+			}else if(sum < s) {
+				sum += a[right++];
+			}
+			if(sum == s) ans++;
+		}
+		
+		System.out.println(ans);
 
-        int sum = 0;
-        int start = 0;
-        int end = 0;
-        int cnt = 0;
-
-        while (end < lists.size()) {
-            if (sum < N) {
-                sum += lists.get(end);
-                if(sum==N){
-                    cnt++;
-                }
-                end++;
-            }
-            if (sum >= N) {
-                sum -= lists.get(start);
-                if(sum==N){
-                    cnt++;
-                }
-                start++;
-            }
-        }
-        System.out.println(cnt);
-    }
+	}
 
 }

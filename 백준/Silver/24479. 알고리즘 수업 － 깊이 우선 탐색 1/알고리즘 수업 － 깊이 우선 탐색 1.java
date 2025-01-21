743841d5af2ab54744071962e8058b604e071b66
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-01-21 18:00:11 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 908 ms, Memory: 89468 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,9 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,43 +12,42 @@ public class Main {
     static int M;
     static int R;
 
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static List<List<Integer>> graph = new ArrayList<>();
     static int[] visited;
     static int cnt = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
         visited = new int[N+1];
 
-        for (int i=1; i<=N+1; i++ ) {
+        for (int i=0; i<N+1; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // 그래프 초기 설정
-        for (int i=1; i<= M; i++) {
+        for (int i=1; i<=M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
 
-            // 양방향 연결
+            // 그래프 초기화
             graph.get(from).add(to);
             graph.get(to).add(from);
         }
 
-        // 오름차순 정렬
-        for (int i=1; i<graph.size(); i++) {
-            Collections.sort(graph.get(i));
+        for (List<Integer> node : graph) {
+            Collections.sort(node);
         }
 
         dfs(R);
 
         StringBuilder sb = new StringBuilder();
-        for (int i= 1;  i< visited.length; i++) {
+        for (int i=1; i<=visited.length-1; i++) {
             sb.append(visited[i]).append("\n");
         }
         System.out.println(sb.toString());

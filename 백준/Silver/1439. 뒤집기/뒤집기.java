8e435560c7d9a6968a289094d88bd79d09920c11
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-08-27 15:50:56 +0900

    [Silver V] Title: 뒤집기, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,31 +1,32 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        char[] chars = input.toCharArray();
-        int cntFirst = 0;
-        int cntSecond = 0;
-        char now = chars[0];
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] != now) {
-                now = chars[i];
-                if (now == '0') {
-                    cntFirst += 1;
+        String s = sc.next();
+
+        int countZero = 0;
+        int countOne = 0;
+
+        if (s.charAt(0) == '0') {
+            countZero ++;
+        } else {
+            countOne ++;
+        }
+
+        for (int i=1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i-1)) {
+                continue;
+            } else {
+                if (s.charAt(i) == '0') {
+                    countZero++;
                 } else {
-                    cntSecond += 1;
+                    countOne++;
                 }
             }
         }
-        if(cntFirst>cntSecond){
-            System.out.println(cntFirst);
-        } else{
-            System.out.println(cntSecond);
-        }
-
+        System.out.println(Math.min(countZero, countOne));
     }
 
 }

a1736bf1b0708fa70f42cc4316ffb863e8dccb1d
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-08-29 19:24:22 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 196 ms, Memory: 17988 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,44 +1,30 @@
-
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    static String underbar = "";
+    static int N;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
+        N = sc.nextInt();
         System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-        recursion(m);
-
+        recursion(0);
     }
 
-
-    static void recursion(int count) {
-
-        String temp = underbar;
-
-        if (count == 0) {
-            System.out.println(temp + "\"재귀함수가 뭔가요?\"");
-            System.out.println(temp+"\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-            System.out.println(temp+"라고 답변하였지.");
+    private static void recursion(int count) {
+        System.out.println("____".repeat(count) + "\"재귀함수가 뭔가요?\"");
+        if (count ==N) {
+            // 여기서 분기 처리
+            System.out.println("____".repeat(count) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println("____".repeat(count) + "라고 답변하였지.");
             return;
         }
+        System.out.println("____".repeat(count) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        System.out.println("____".repeat(count) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        System.out.println("____".repeat(count) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
 
-        System.out.println(temp + "\"재귀함수가 뭔가요?\"");
-        System.out.println(temp + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-        System.out.println(temp + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-        System.out.println(temp + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-
-        underbar += "____";
-        
-        recursion(count - 1);
-        System.out.println(temp+"라고 답변하였지.");
+        recursion(count + 1);
+        System.out.println("____".repeat(count) + "라고 답변하였지.");
     }
+
 }

83e39dbfd0d562b9cdbad96b156cfa5b6b460161
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-08-29 18:52:09 +0900

    [Bronze III] Title: 팩토리얼, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 public class Main {
 
-    static int sum = 1;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        recursion(N);
+        int sum = 1;
+        for (int i=N; i>0; i--) {
+            sum *= i;
+        }
         System.out.println(sum);
     }
 
-    private static void recursion(int count) {
-        if (count == 0) {
-            return;
-        }
-        sum *= count;
-        recursion(count - 1);
-    }
 }

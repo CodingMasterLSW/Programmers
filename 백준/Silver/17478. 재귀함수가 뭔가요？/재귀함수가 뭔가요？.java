c64b8eb9d9764e3b9bb53442daf65c64110c92f5
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-08-29 19:37:39 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 192 ms, Memory: 17920 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,19 +12,21 @@ public class Main {
     }
 
     private static void recursion(int count) {
-        System.out.println("____".repeat(count) + "\"재귀함수가 뭔가요?\"");
-        if (count ==N) {
-            // 여기서 분기 처리
+        if (count == N) {
+            System.out.println("____".repeat(count) + "\"재귀함수가 뭔가요?\"");
             System.out.println("____".repeat(count) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
             System.out.println("____".repeat(count) + "라고 답변하였지.");
             return;
         }
+
+        System.out.println("____".repeat(count) + "\"재귀함수가 뭔가요?\"");
         System.out.println("____".repeat(count) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
         System.out.println("____".repeat(count) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
         System.out.println("____".repeat(count) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
 
         recursion(count + 1);
         System.out.println("____".repeat(count) + "라고 답변하였지.");
+
     }
 
 }

69dc7543cfe2d40c35b1c535d8dd817bfae12b79
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-11-01 22:41:21 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,63 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
 
     static int N;
     static int M;
     static int[][] graph;
-    static boolean[][] visited;
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {-1,1,0,0};
+    static int[][] resultGraph;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         graph = new int[N][M];
-        visited = new boolean[N][M];
+        resultGraph = new int[N][M];
 
-        for(int i=0; i<N; i++){
-            String s = br.readLine();
-            for(int j=0; j<M; j++){
-                graph[i][j] = s.charAt(j) - '0';
+        for (int i=0; i<N; i++) {
+            String input = br.readLine();
+            for (int j=0; j<M; j++) {
+                graph[i][j] = input.charAt(j) - '0';
             }
         }
-        System.out.println(bfs(0,0));
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                resultGraph[i][j] = -1;
+            }
+        }
+
+        bfs(0, 0);
+        System.out.println(resultGraph[N-1][M-1] + 1);
     }
 
-    public static int bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x,y});
-        visited[x][y] = true;
+    public static void bfs(int startDr, int startDc) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{startDr, startDc});
+        resultGraph[startDr][startDc] = 0;
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int[] current = q.poll();
-            int currentX = current[0];
-            int currentY = current[1];
+            for (int i=0; i<4; i++) {
+                int ndr = current[0] + dr[i];
+                int ndc = current[1] + dc[i];
 
-            for(int i=0; i<4; i++){
-                int ndx = currentX + dx[i];
-                int ndy = currentY + dy[i];
-
-                if (ndx < 0 || ndx >= N || ndy < 0 || ndy >= M) {
-                    continue;
-                }
-                if (visited[ndx][ndy] || graph[ndx][ndy] != 1){
+                if (ndr<0 || ndr >= N || ndc < 0 || ndc >= M) {
                     continue;
                 }
 
-                visited[ndx][ndy] = true;
-                q.offer(new int[]{ndx,ndy});
-
-                graph[ndx][ndy] = graph[currentX][currentY]+1;
-
-                if(ndx == N-1 && ndy == M-1){
-                    return graph[ndx][ndy];
+                if (graph[ndr][ndc] == 0 || resultGraph[ndr][ndc] != -1) {
+                    continue;
                 }
+                resultGraph[ndr][ndc] = resultGraph[current[0]][current[1]] + 1;
 
+                q.offer(new int[]{ndr, ndc});
             }
         }
-        return -1;
     }
 }

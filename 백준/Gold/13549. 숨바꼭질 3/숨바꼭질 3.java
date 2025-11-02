82b0623f493e67c0b882410f5db1cc95e529872f
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-11-02 20:11:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 19828 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N;
+    static int M;
+    static boolean[] visited = new boolean[200001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(N));
+    }
+
+    public static int bfs(int start) {
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{start, 0});
+        visited[start] = true;
+
+        while(!q.isEmpty()) {
+            int[] currentInfos = q.poll();
+            int currentLocation = currentInfos[0];
+            int currentTime = currentInfos[1];
+
+            if (currentLocation == M) {
+                return currentTime;
+            }
+            
+            if (currentLocation * 2 >= 0 && currentLocation * 2 <=200_000 && !visited[currentLocation * 2]) {
+                q.addFirst(new int[]{currentLocation * 2, currentTime});
+                visited[currentLocation * 2] = true;
+            }
+            
+            if (currentLocation + 1 <= 200000 && currentLocation - 1 >= 0 && !visited[currentLocation - 1]) {
+                q.addLast(new int[]{currentLocation - 1, currentTime + 1});
+                visited[currentLocation - 1] = true;
+            }
+
+            if (currentLocation + 1 <= 200000 && currentLocation + 1 >= 0 && !visited[currentLocation + 1]) {
+                q.addLast(new int[]{currentLocation + 1, currentTime + 1});
+                visited[currentLocation + 1] = true;
+            }
+            
+        }
+
+        return -1;
+    }
+}

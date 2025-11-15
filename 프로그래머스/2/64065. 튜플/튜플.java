e471f48d009cf81425334631b18c9a517d41980d
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-11-15 15:05:45 +0900

    [level 2] Title: 튜플, Time: 57.10 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,39 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
+        String[] split = s.replaceAll("[{}]", " ").trim().split(" ,");
         
-        List<int[]> arr = new ArrayList<>();
+        Set<String> arr = new HashSet<>();
         
-        String[] newS = s.replaceAll("[{}]", " ").split(" ,");
-        for (int i=0; i<newS.length; i++) {
-            String[] split = newS[i].trim().split(",");    
-            int[] nums = new int[split.length];
-            for (int j = 0; j < split.length; j++) {
-                nums[j] = Integer.parseInt(split[j]);
-            }
-            arr.add(nums);
-        }
+        Arrays.sort(split, (o1, o2) -> o1.length() - o2.length());
         
-        Collections.sort(arr, (o1, o2) -> o1.length - o2.length);
+        int[] answer = new int[split.length];
         
-        List<Integer> result = new ArrayList<>();
-        for (int[] a : arr) {
-            for (int i=0; i<a.length; i++) {
-                if (result.contains(a[i])) {
-                    continue;
-                } else {
-                    result.add(a[i]);
+        int idx = 0;
+        
+        for (int i=0; i<split.length; i++) {
+            String[] split2 = split[i].split(",");
+            for (int j=0; j<split2.length; j++) {
+                String value = split2[j].trim();
+                if (arr.add(value)) {
+                    answer[idx] = Integer.parseInt(value);
+                    idx++;
                     break;
                 }
             }
         }
-        int[] finalResult = new int[result.size()];
-        
-        for (int i=0; i<result.size(); i++) {
-            finalResult[i] = result.get(i);
-        }
-
-        
-        return finalResult;
+        return answer;
     }
 }

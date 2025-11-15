50b456df7e452fda22f7d8f2be7abbf1ce0f55e0
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-11-15 20:49:38 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.22 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,26 +1,19 @@
 import java.util.*;
 
 class Solution {
+    static List<Direction> phone = new ArrayList<>();
     
-    static char[][] phone = new char[4][3];
-    static boolean[][] visited = new boolean[4][3];
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static Direction currentLeftDirection;
+    static Direction currentRightDirection;
     
-    static int[] currentLeftHand = new int[]{3, 0};
-    static int[] currentRightHand = new int[]{3, 2};
-
     public String solution(int[] numbers, String hand) {
-        initPhone();
+        init();
+        
+        currentLeftDirection = phone.get(10);
+        currentRightDirection = phone.get(11);   
+        
         StringBuilder sb = new StringBuilder();
         for (int i=0; i<numbers.length; i++) {
-            // System.out.println(
-            //     "왼손 위치: " + phone[currentLeftHand[0]][currentLeftHand[1]]
-            // );
-            // System.out.println(
-            //     "오른손 위치: " + phone[currentRightHand[0]][currentRightHand[1]]
-            // );
-            // System.out.println();
             sb.append(calculate(numbers[i], hand));
         }
         return sb.toString();
@@ -28,93 +21,73 @@ class Solution {
     
     public static String calculate(int number, String hand) {
         if (number == 1 || number == 4 || number == 7) {
-            updateHand(number, "left");
+            currentLeftDirection = phone.get(number);
             return "L";
         }
+        
         if (number == 3 || number == 6 || number == 9) {
-            updateHand(number, "right");
+            currentRightDirection = phone.get(number);
             return "R";
         }
-        visited = new boolean[4][3];
-        int distance1 = bfs(number, currentLeftHand);
-        //System.out.println("왼손 거리 : " + distance1);
         
-        visited = new boolean[4][3];
-        int distance2 = bfs(number, currentRightHand);
-        //System.out.println("오른손 거리 : " + distance2);
+        Direction targetDirection = phone.get(number);
         
-        if (distance1 == distance2) {
-            updateHand(number, hand);
-            return String.valueOf(Character.toUpperCase(hand.charAt(0)));
+        int leftResult = Math.abs(targetDirection.getX() - currentLeftDirection.getX()) + Math.abs(targetDirection.getY() - currentLeftDirection.getY());
+        
+        int rightResult = Math.abs(targetDirection.getX() - currentRightDirection.getX()) + Math.abs(targetDirection.getY() - currentRightDirection.getY());
+        
+        if (leftResult == rightResult) {
+            if (hand.equals("right")) {
+                currentRightDirection = phone.get(number);
+            } else {
+                currentLeftDirection = phone.get(number);
+            }
+            return String.valueOf(hand.toUpperCase().charAt(0));
         }
         
-        if (distance1 < distance2) {
-            updateHand(number, "left");
+        if (leftResult > rightResult) {
+            currentRightDirection = phone.get(number);
+            return "R";
+        } else {
+            currentLeftDirection = phone.get(number);
             return "L";
         }
-        updateHand(number, "right");
-        return "R";
     }
     
-    public static int bfs(int target, int[] start) {
-        //System.out.println("bfs 시작");
-        //System.out.println("시작 좌표 : " + start[0] + " " + start[1]);
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{start[0], start[1], 0});
+    public static void init() {
+        phone.add(new Direction(3, 1));
         
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            
-            if (phone[current[0]][current[1]] == (char)(target + '0')) {
-                return current[2];
-            }
-            
-            for (int i=0; i<4; i++) {
-                int ndx = current[0] + dx[i];
-                int ndy = current[1] + dy[i];
-                
-                if (ndx < 0 || ndx > 3 || ndy < 0 || ndy > 2) {
-                    continue;
-                }
-                
-                if (visited[ndx][ndy]) {
-                    continue;
-                }
-                visited[ndx][ndy] = true;
-                q.offer(new int[]{ndx, ndy, current[2] + 1});
-            }
-        }
-        return -1;
-    }
-    
-    public static void updateHand(int number, String hand) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (phone[i][j] == (char)(number + '0')) {
-                if (hand.equals("right")) {
-                    currentRightHand = new int[]{i, j};
-                } else {
-                    currentLeftHand = new int[]{i, j};
-                }
-                return;
-            }
-        }
+        phone.add(new Direction(0, 0));
+        phone.add(new Direction(0, 1));
+        phone.add(new Direction(0, 2));
+        
+        phone.add(new Direction(1, 0));
+        phone.add(new Direction(1, 1));
+        phone.add(new Direction(1, 2));
+        
+        phone.add(new Direction(2, 0));
+        phone.add(new Direction(2, 1));
+        phone.add(new Direction(2, 2));
+        
+        phone.add(new Direction(3, 0));
+        phone.add(new Direction(3, 2));
     }
 }
 
+class Direction {
+    int x;
+    int y;
     
-    public static void initPhone() {
-        phone[0][0] = '1';
-        phone[0][1] = '2';
-        phone[0][2] = '3';
-        phone[1][0] = '4';
-        phone[1][1] = '5';
-        phone[1][2] = '6';
-        phone[2][0] = '7';
-        phone[2][1] = '8';
-        phone[2][2] = '9';
-        phone[3][0] = '*';
-        phone[3][1] = '0';
-        phone[3][2] = '#';
+    public Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
     }
 }

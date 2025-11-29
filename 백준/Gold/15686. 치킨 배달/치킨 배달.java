1938ac28b77ae33da4e7aed2e9ca136fad9dcde3
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-11-29 11:40:27 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 18912 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,27 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
-public class Main {
-
+import java.io.*;
+import java.util.*;
+class Main {
+    
     static int N;
     static int M;
     static int[][] graph;
-    static List<int[]> answer = new ArrayList<>();
-    static boolean[] visited;
-    static int currentMin = Integer.MAX_VALUE;
-    static List<int[]> homes = new ArrayList<>();
-    static List<int[]> chickens = new ArrayList<>();
+    static int minValue;
+    static List<int[]> currentChicken = new ArrayList<>();
+    static List<int[]> startPoint = new ArrayList<>();
+    static List<int[]> chickenPoint = new ArrayList<>();
+    static int finalResult = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         graph = new int[N][N];
 
         for (int i=0; i<N; i++) {
@@ -29,39 +23,45 @@ public class Main {
             for (int j=0; j<N; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
                 if (graph[i][j] == 1) {
-                    homes.add(new int[] {i, j});
-                } else if (graph[i][j] == 2) {
-                    chickens.add(new int[] {i, j});
+                    startPoint.add(new int[]{i,j});
+                }
+                if (graph[i][j] == 2) {
+                    chickenPoint.add(new int[] {i,j});
                 }
             }
         }
-        visited = new boolean[chickens.size()];
-        dfs(0, 0);
-        System.out.println(currentMin);
+        backtracking(0);
+        System.out.println(finalResult);
+
     }
 
-    public static void dfs(int depth, int start) {
-        if (depth == M) {
-            int sum = 0;
-            for (int[] home : homes) {
-                int cal = Integer.MAX_VALUE;
-                for (int[] ans : answer) {
-                    cal = Math.min(cal, (Math.abs(ans[0] - home[0]) + Math.abs(ans[1] - home[1])));
-                }
-                sum += cal;
-            }
-            currentMin = Math.min(sum, currentMin);
+    public static void backtracking(int start) {
+        if (currentChicken.size() == M) {
+            cal();
             return;
         }
 
-        for (int i = start; i < chickens.size(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                answer.add(chickens.get(i));
-                dfs(depth + 1, i+1);
-                visited[i] = false;
-                answer.remove(answer.size()-1);
-            }
+        for (int i=start; i<chickenPoint.size(); i++) {
+            int[] target = chickenPoint.get(i);
+            currentChicken.add(target);
+            backtracking(i + 1);
+            currentChicken.remove(target);
         }
+    }
+
+    public static void cal() {
+        // startPoint -> currentChicken 거리를 전부 돌아야해요.
+        // -> 그러니깐, 2중 반복문이 필요하다.
+        int result = 0;
+
+        for (int[] start : startPoint) {
+            int minValue = Integer.MAX_VALUE;
+            for (int[] cur : currentChicken) {
+                int calValue = Math.abs(start[0] - cur[0]) + Math.abs(start[1] - cur[1]);
+                minValue = Math.min(minValue, calValue);
+            }
+            result += minValue;
+        }
+        finalResult = Math.min(result, finalResult);
     }
 }

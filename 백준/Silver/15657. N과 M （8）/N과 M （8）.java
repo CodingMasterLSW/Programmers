010935468b7f9d587dfe6f6829a0418385bbf662
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-12-22 12:40:54 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 21652 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,47 +1,55 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-
+class Main {
+    
     static int N;
     static int M;
-    static boolean[] visited;
+    static int[] result;
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        visited = new boolean[N];
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
-        int[] resources = new int[M];
+        result = new int[M];
 
-        backtracking(N, M, visited, resources, 0, 0);
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        backtracking(0, 0);
         System.out.println(sb.toString());
     }
 
-    public static void backtracking(int n, int m, boolean[] visited, int[] resources, int depth,
-            int start) {
+    public static void backtracking(int depth, int start) {
+        // 종료조건
         if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(resources[i] + " ");
+            for (int i=0; i<M; i++) {
+                sb.append(result[i] + " ");
             }
             sb.append("\n");
             return;
         }
-
-        for (int i = start; i < N; i++) {
-            resources[depth] = arr[i];
-            backtracking(n, m, visited, resources, depth + 1, i);
+    
+        for (int i=start; i<arr.length; i++) {
+            // 이미 방문했니?
+            // if (visited[i]) {
+            //     continue;
+            // }
+        
+            // if (i > 0 && !visited[i-1] && arr[i-1] == arr[i]) {
+            //     continue;
+            // }
+            
+            result[depth] = arr[i];
+            backtracking(depth + 1, i);
         }
-
 
     }
 }
-

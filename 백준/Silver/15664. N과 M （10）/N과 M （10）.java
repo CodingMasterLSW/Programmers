b0e187767ce48fac39f4118076da6e301f9db743
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-12-22 12:27:59 +0900

    [Silver II] Title: N과 M (10), Time: 116 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -1,48 +1,59 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-
+class Main {
+    
     static int N;
     static int M;
+    static int[] result;
     static int[] arr;
-    static StringBuilder sb;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         arr = new int[N];
-        sb = new StringBuilder();
+        result = new int[M];
+        visited = new boolean[N];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-
-        int[] resources = new int[M];
-        backtracking(N, M, resources, 0, 0);
+        backtracking(0, 0);
         System.out.println(sb.toString());
-
     }
 
-    public static void backtracking(int n, int m, int[] resources, int depth, int start) {
-        if (M == depth) {
-            for (int i = 0; i < M; i++) {
-                sb.append(resources[i] + " ");
+    public static void backtracking(int depth, int start) {
+        // 종료조건
+        if (depth == M) {
+            for (int i=0; i<M; i++) {
+                sb.append(result[i] + " ");
             }
             sb.append("\n");
             return;
         }
-        Set<Integer> used = new HashSet<>();
-        for (int i = start; i < N; i++) {
-            if (!used.contains(arr[i])) {
-                resources[depth] = arr[i];
-                backtracking(N, M, resources, depth + 1, i + 1);
-                used.add(arr[i]);
+    
+        for (int i=start; i<arr.length; i++) {
+            // 이미 방문했니?
+            if (visited[i]) {
+                continue;
             }
-
+        
+            if (i > 0 && !visited[i-1] && arr[i-1] == arr[i]) {
+                continue;
+            }
+            
+            visited[i] = true;
+            result[depth] = arr[i];
+            backtracking(depth + 1, i+1);
+            visited[i] = false;
         }
-    }
 
+    }
 }

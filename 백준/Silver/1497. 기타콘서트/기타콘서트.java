01ec23625c753edf7b09e669a2ebb0b8d4284bc7
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-12-22 16:29:58 +0900

    [Silver I] Title: 기타콘서트, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.java
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.java
@@ -5,9 +5,9 @@ class Main {
 
     static int N;
     static int M;
-    static boolean[] visited;
-    static String[] guitar;
-    static int maxCount = 0;
+    static String[] guitars;
+    static int targetCount = 0;
+    static boolean[] visitedGuitar;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,83 +15,87 @@ class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        guitar = new String[N];
-        visited = new boolean[N];
+        guitars = new String[N];
+        visitedGuitar = new boolean[N];
 
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             st.nextToken();
-            guitar[i] = st.nextToken();
+            guitars[i] = st.nextToken();
         }
         
-        int current = 1;
-        int prevMaxCount = -1;
-        
-        while(true) {
-            if (current > N) {
-                if (maxCount == 0) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(current - 1);
+        boolean[] canPlay = new boolean[M];
+       
+        // targetCount 초기화
+        for (int i=0; i<N; i++) {
+            String guitar = guitars[i];
+            for (int j=0; j<M; j++) {
+                if (guitar.charAt(j) == 'Y') {
+                    canPlay[j] = true;
                 }
-                break;
             }
-            
-            backtracking(0, 0, current);
-            
-            if (maxCount == prevMaxCount) {
-                if (maxCount == 0) {
-                    System.out.println("-1");
-                } else {
-                    System.out.println(current -1);
-                }
-                break;
-            }
-            
-            prevMaxCount = maxCount;
-            current++;
         }
+
+        for (int i=0; i<M; i++) {
+            if (canPlay[i]) {
+                targetCount ++;
+            }
+        }
+        // 아무것도 없는 경우
+        if (targetCount==0) {
+            System.out.println("-1");
+            return;
+        }
+
+        int currentDepth = 1;
+        while (true) {
+
+            boolean result = backtracking(0, 0, currentDepth);
+            if (result) {
+                System.out.println(currentDepth);
+                break;
+            } else {
+                currentDepth ++;
+            }
+        }
+    
     }
 
-    public static boolean backtracking(int depth, int start, int currentTracking) {
-        if (depth == currentTracking) {
-            List<String> currentGuitar = new ArrayList<>();
+    public static boolean backtracking(int depth, int start, int currentDepth) {
+        // 종료 조건
+        // 여기서 visited 계산 후, targetCount와 같은지 비교한다
+        // 같으면 true, 아니면 false를 반환한다.
+        if (currentDepth == depth) {
+            boolean[] tmpCanPlay = new boolean[M];
             for (int i=0; i<N; i++) {
-                if (visited[i]) {
-                    currentGuitar.add(guitar[i]);
+                if (visitedGuitar[i]) {
+                    String current = guitars[i];
+                    for (int j=0; j<M; j++) {
+                        if (current.charAt(j) == 'Y') {
+                            tmpCanPlay[j] = true;
+                        }
+                    }
+                    
                 }
             }
-
-            boolean[] canPlay = new boolean[M];
-
-            for (String guitar :  currentGuitar) {
-                for (int i=0; i<guitar.length(); i++) {
-                    char play = guitar.charAt(i);
-                    if (play == 'Y') {
-                        canPlay[i] = true;
+            int tmpCount = 0;
+                for (int j=0; j<M; j++) {
+                    if (tmpCanPlay[j]) {
+                        tmpCount++;
                     }
                 }
-            }
-            
-            int count = 0;
-            for (int i=0; i<M; i++) {
-                if (canPlay[i]) {
-                    count++;
-                }
-            }
-            
-            maxCount = Math.max(maxCount, count);
-            return false;
+                if (tmpCount== targetCount) {
+                    return true;
+                }  
+        return false;
         }
 
         for (int i=start; i<N; i++) {
-            if (visited[i]) {
-                continue;
+            visitedGuitar[i] = true;
+            if (backtracking(depth+1, i+1, currentDepth)) {
+                return true;
             }
-
-            visited[i] = true;
-            backtracking(depth+1, i+1, currentTracking);
-            visited[i] = false;
+            visitedGuitar[i] = false;
         }
         return false;
     }

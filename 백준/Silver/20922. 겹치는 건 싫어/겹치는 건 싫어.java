bae6d58e843a75e240b99db43fdb959c59fd1372
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-12-30 16:19:33 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 536 ms, Memory: 64876 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N;
+    static int M;
+    static int[] arr;
+    static int maxCount = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int startIdx = 0;
+        int endIdx = 0;
+        int count = 0;
+
+        Map<Integer, Integer> histories = new HashMap<>();
+
+        while(endIdx < N) {
+            histories.put(arr[endIdx], histories.getOrDefault(arr[endIdx], 0) + 1);
+
+            if (histories.get(arr[endIdx]) > M) {
+                while (true) {
+                    histories.put(arr[startIdx], histories.get(arr[startIdx]) - 1);
+                    startIdx++;
+                    count--;
+                    if (histories.get(arr[endIdx]) <= M) {
+                        break;
+                    }
+                }
+            } 
+            count++;
+            maxCount = Math.max(maxCount, count);
+            endIdx++;
+        }
+        System.out.println(maxCount);
+    }
+}

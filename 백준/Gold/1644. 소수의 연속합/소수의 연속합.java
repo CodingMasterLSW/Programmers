bf788c19650b8aae9333cdd0bea8908785d8d93f
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-01-02 00:01:29 +0900

    [Gold III] Title: 소수의 연속합, Time: 212 ms, Memory: 38708 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -2,72 +2,72 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
-    static int[] arr = new int[4000005];
+    
     static int N;
-    static List<Integer> prime = new ArrayList<>();
+    static int[] arr = new int[4000005];
+    static List<Integer> primes = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
         init();
-        
+
         int startIdx = 0;
         int endIdx = 0;
-        
         int sum = 0;
-        int count = 0;
+        int cnt = 0;
 
-        while(startIdx < prime.size() && prime.get(startIdx) <= N) {
-            if (sum <= N && endIdx < prime.size()) {
-                sum += prime.get(endIdx);
-                endIdx ++;
+        if (N == 2) {
+            System.out.println(1);
+            return;
+        }
+
+        while (true) {
+            if (sum >= N) {
                 if (sum == N) {
-                    count++;
+                    cnt++;
                 }
-
-            } else if (sum > N) {
-                while (true) {
-                    if (sum <= N) {
-                        if (sum == N) {
-                            count++;
-                        }
-                        break;
-                    }
-                    sum -= prime.get(startIdx);
-                    //System.out.println("sum = " + sum);
-
-                    startIdx++;
-                }
-            } else {
+                sum -= primes.get(startIdx);
+                startIdx++;
+            } else if (endIdx == primes.size()) {
                 break;
-            }
-            //System.out.println("sum = " + sum);
-        }
-        System.out.println(count);
-    }
-
-    public static void init() {
-        for (int i=2; i<= 4_000_000; i++) {
-            arr[i] = i;
-        }
-        
-        for (int i=2; i*i<=4_000_000; i++) {
-            if (arr[i] == 0) {
-                continue;
-            }
-
-            for (int j = i+i; j <= 4_000_000; j+=i) {
-                arr[j] = 0;
+            } else {
+                sum += primes.get(endIdx);
+                endIdx++;
             }
             
         }
+        System.out.println(cnt);
+    }
 
-        for (int i=1; i<=4_000_000; i++) {
+    public static void init() {
+        for (int i=2; i<=N; i++) {
+            arr[i] = i;
+        }
+
+        for (int i=2; i*i<=N; i++) {
+            if (arr[i] == 0) {
+                continue;
+            }
+            int count = 2;
+            while (true) {
+                int current = count * i;
+
+                if (current > 4_000_000) {
+                    break;
+                } else {
+                    arr[current] = 0;
+                    count++;
+                }
+            }
+        }
+
+        for (int i=2; i<=4_000_000; i++) {
             if (arr[i] != 0) {
-                prime.add(arr[i]);
+                primes.add(arr[i]);
             }
         }
     }
+
+
 }

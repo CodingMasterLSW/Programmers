4c6a815137e0eb13df1d7950c0624514e351e156
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-01-06 15:58:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 636 ms, Memory: 59220 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,38 +1,42 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+class Main {
 
-public class Main {
-
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N+1];
+        dp = new int[N+1];
+
         st = new StringTokenizer(br.readLine());
 
-        for (int i=0; i<arr.length; i++) {
+        for (int i=1; i<=N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] += dp[i-1] + arr[i];
         }
 
-        // dp 초기화
-        int[] dp = new int[N+1];
-        dp[1] = arr[0];
-        for (int i=2; i<=N; i++) {
-            dp[i] = dp[i-1] + arr[i-1];
-        }
-        
         for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            int result = dp[end] - dp[start - 1];
-            System.out.println(result);
+            if (a == 1) {
+                sb.append(dp[b] + "\n");
+            } else {
+                sb.append(dp[b] - dp[a-1] + "\n");
+            }
         }
+        System.out.println(sb.toString());
     }
 }

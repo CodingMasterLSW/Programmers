71a76bfc842141105556cfb4d92fb080ef4fc2fc
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-01-06 15:38:01 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 58.44 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,15 +1,9 @@
-// 파괴되었다가 복구 가능
-// 공격은 직사각형 형태 -> bfs로 할 필요가 없어보임. 그냥 구현 아닌가
-import java.util.*;
-
 class Solution {
-    
     public int solution(int[][] board, int[][] skill) {
-        
         int n = board.length;
         int m = board[0].length;
         
-        int[][] copyBoard = new int[n+1][m+1];
+        int[][] sum = new int[n+1][m+1];
         
         for (int[] current : skill) {
             int type = current[0];
@@ -20,35 +14,40 @@ class Solution {
             int degree = current[5];
             
             if (type == 1) {
-                copyBoard[x1][y1] -= degree;
-                copyBoard[x1][y2 + 1] += degree;
-                copyBoard[x2 + 1][y1] += degree;
-                copyBoard[x2 + 1][y2 + 1] -= degree;
+                // 파괴 구현
+                sum[x1][y1] += -degree;
+                sum[x1][y2+1] += degree;
+                sum[x2+1][y1] += degree;
+                sum[x2+1][y2+1] += -degree;
+                
             } else {
-                copyBoard[x1][y1] += degree;
-                copyBoard[x1][y2 + 1] -= degree;
-                copyBoard[x2 + 1][y1] -= degree;
-                copyBoard[x2 + 1][y2 + 1] += degree;
+                // 회복 구현
+                sum[x1][y1] += degree;
+                sum[x1][y2+1] += -degree;
+                sum[x2+1][y1] += -degree;
+                sum[x2+1][y2+1] += degree;
             }
         }
         
-        for (int i=0; i<=n; i++) {
-            for (int j=1; j<=m; j++) {
-                copyBoard[i][j] += copyBoard[i][j-1];
+        // 가로 누적합 계산
+        for (int i=0; i<n+1; i++) {
+            for (int j=1; j<m+1; j++) {
+                sum[i][j] += sum[i][j-1];
             }
         }
         
-        for (int j=0; j<=m; j++) {
-            for (int i=1; i<=n; i++) {
-                copyBoard[i][j] += copyBoard[i-1][j];
+        // 세로 누적합 계산
+        for (int j=0; j<m+1; j++) {
+            for (int i=1; i<n+1; i++) {
+                sum[i][j] += sum[i-1][j];
             }
         }
-     
+        
         
         int cnt = 0;
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
-                if (board[i][j] + copyBoard[i][j] >= 1) {
+                if (board[i][j] + sum[i][j] >= 1) {
                     cnt++;
                 }
             }

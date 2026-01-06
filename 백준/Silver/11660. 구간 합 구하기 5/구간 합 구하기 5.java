16315c36fada4372a66e4df3f184654b3310850e
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-01-07 00:10:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 880 ms, Memory: 118728 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,16 +1,13 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
-
+class Main {
 
     static int N;
     static int M;
     static int[][] graph;
-
+    static int[][] dp;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,9 +16,8 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         graph = new int[N][N];
+        dp = new int[N+1][N+1];
 
-
-        // 그래프 초기화
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<N; j++) {
@@ -29,32 +25,31 @@ public class Main {
             }
         }
 
-        int[][] dp = new int[N+1][N+1];
-
         for (int i=1; i<=N; i++) {
-            dp[i][1] = graph[i-1][0];
-        }
-
-        // dp 초기화
-        for (int i=1; i<=N; i++) {
-            for (int j=2; j<=N; j++) {
-                dp[i][j] = dp[i][j-1] + graph[i-1][j-1];
+            for (int j=1; j<=N; j++) {
+                dp[i][j] += dp[i][j-1] + graph[i-1][j-1];
             }
         }
 
         for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
+            // 여기서 로직 필요
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int sum = 0;
-            for (int start=x1; start<=x2; start++) {
-                sum += dp[start][y2] - dp[start][y1-1];
-            }
-            System.out.println(sum);
-        }
+            int minX = Math.min(x1, x2);
+            int maxX = Math.max(x1, x2);
+            int minY = Math.min(y1, y2);
+            int maxY = Math.max(y1, y2);
 
+            int result = 0;
+            for (int j=minX; j<=maxX; j++) {
+                result += dp[j][maxY] - dp[j][minY-1];
+            }
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }

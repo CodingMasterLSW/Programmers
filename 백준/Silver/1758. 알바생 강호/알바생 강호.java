24a975281e999b01d44010b5d74b14094d099cba
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-01-15 14:13:51 +0900

    [Silver IV] Title: 알바생 강호, Time: 236 ms, Memory: 24840 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    static int N;
+    static List<Integer> arr = new ArrayList<>();
+    static long sum = 0;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        arr.sort(Comparator.reverseOrder());
+
+        for (int i=0; i<N; i++) {
+            if (arr.get(i) - i < 0) {
+                continue;
+            } else {
+                sum += arr.get(i) - i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}

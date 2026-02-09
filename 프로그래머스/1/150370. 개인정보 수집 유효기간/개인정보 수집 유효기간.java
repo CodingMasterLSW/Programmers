4679570907e2b1bb63210e34cdf1f68c89066fd4
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-02-09 19:29:38 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 10.40 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,72 +1,52 @@
+// Map으로 약관 저장
+// privacies 일자로 치환 후 계산하기
+// today 일자로 치환 후 계산하기
+
 import java.util.*;
 
 class Solution {
     
-    static Map<String, Integer> rules = new HashMap<>();
+    static Map<String, Integer> type = new HashMap<>();
     
     public int[] solution(String today, String[] terms, String[] privacies) {
         for (String term : terms) {
             String[] splitTerm = term.split(" ");
-            rules.put(splitTerm[0], Integer.parseInt(splitTerm[1]));
+            type.put(splitTerm[0], Integer.parseInt(splitTerm[1]));
         }
         
-        int cnt = 0;
         List<Integer> result = new ArrayList<>();
-        for (String privacy : privacies) {
+        for (int i=0; i<privacies.length; i++) {
+            String privacy = privacies[i];
+            
             String[] splitPrivacy = privacy.split(" ");
+            
             String date = splitPrivacy[0];
-            String rule = splitPrivacy[1];
+            String keyType = splitPrivacy[1];
+            
+            int addValue = type.get(keyType);
             
             String[] splitDate = date.split("\\.");
+            
             int year = Integer.parseInt(splitDate[0]);
             int month = Integer.parseInt(splitDate[1]);
-            int day = Integer.parseInt(splitDate[2]);           
+            int day = Integer.parseInt(splitDate[2]);
             
-            // System.out.println("기존 year = " + year);
-            // System.out.println("기존 month = " + month);
-            // System.out.println("기존 day = " + day);
-            int monthPeriod = rules.get(rule);
-            int dueMonth = month + monthPeriod;
+            int privacyValue = (year * 28 * 12) + (month * 28) + day + addValue * 28;
             
-            // System.out.println("monthPeriod = " + monthPeriod);
-            month += monthPeriod;
-            year += (month - 1) / 12;
-            month = (month - 1) % 12 + 1;
-            
-            if (day == 1) {
-                month -=1;
-                day = 28;
-                if (month == 0) {
-                    year -= 1;
-                    month = 12;
-                }
-            } else {
-                day --;
-            }
-        
-            // System.out.println(year);
-            // System.out.println(month);
-            // System.out.println(day);
-            // System.out.println();
             
             String[] todaySplit = today.split("\\.");
-            int todayYear = Integer.parseInt(todaySplit[0]);
-            int todayMonth = Integer.parseInt(todaySplit[1]);
-            int todayDay = Integer.parseInt(todaySplit[2]);
+            int year2 = Integer.parseInt(todaySplit[0]);
+            int month2 = Integer.parseInt(todaySplit[1]);
+            int day2 = Integer.parseInt(todaySplit[2]);
             
-            int sum = (year * 10000 + month * 100 + day * 1);
-            int todaySum = (todayYear * 10000 + todayMonth * 100 + todayDay * 1);
+            int todayValue = (year2 * 28 * 12) + (month2 * 28) + day2;
             
-            if (sum < todaySum) {
-                result.add(cnt+1);
+      
+            if (privacyValue <= todayValue) {
+                result.add(i+1);
             }
-            cnt++;
         }
-        System.out.println(result);
-        int[] realResult = new int[result.size()];
-        for (int i=0; i<result.size(); i++) {
-            realResult[i] = result.get(i);
-        }
-        return realResult;
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }

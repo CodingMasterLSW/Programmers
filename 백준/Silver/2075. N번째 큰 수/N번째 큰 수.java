e1d28147cd48d8be987e901b4e44e0214911674a
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-02-15 18:56:10 +0900

    [Silver III] Title: N번째 큰 수, Time: 792 ms, Memory: 244068 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -2,22 +2,24 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
-    static int N;
-
     public static void main(String[] args) throws IOException {
-        List<Integer> arr = new ArrayList<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int N = Integer.parseInt(br.readLine());
 
         for (int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j=0; j<N; j++) {
-                arr.add(Integer.parseInt(st.nextToken()));
+                int current = Integer.parseInt(st.nextToken());
+                pq.offer(current);
+
+                if (pq.size() > N) {
+                    pq.poll();
+                }
             }
         }
-        Collections.sort(arr);
-        System.out.println(arr.get(arr.size() - N));
+    
+        System.out.println(pq.peek());
     }
 }

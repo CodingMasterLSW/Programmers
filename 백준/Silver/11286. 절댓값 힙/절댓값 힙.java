1ef70b8e242e10e237424599bbdc57ffc3ab0ffb
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-02-15 18:14:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 276 ms, Memory: 25356 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,47 +1,36 @@
+import java.io.*;
+import java.util.*;
 
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
-public class Main {
-
-    static PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> {
-        int absCompare = Integer.compare(Math.abs(a), Math.abs(b));
-        if (absCompare == 0) {
-            return Integer.compare(a, b);
-        } else {
-            return absCompare;
-        }
-    });
-
+class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> {
+            int absA = Math.abs(a); // -2
+            int absB = Math.abs(b); // 2
 
-        int m = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < m; i++) {
-            int x = Integer.parseInt(br.readLine());
-
-            if (x == 0) {
-                if (q.isEmpty()) {
-                    bw.write(String.valueOf(0));
-                    bw.newLine();
-                } else {
-                    bw.write(String.valueOf(q.poll()));
-                    bw.newLine();
-                }
-            } else {
-                q.add(x);
+            if (absA - absB == 0) {
+                return a - b;
             }
-        }
-        bw.flush();
-        bw.close();
-    }
+            return absA - absB;
+        });
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            int current = Integer.parseInt(br.readLine());
+            if (current == 0) {
+                if (minHeap.isEmpty()) {
+                    sb.append(0).append("\n");
+                    continue;
+                }
+                sb.append(minHeap.poll()).append("\n");
+                continue;
+            }
+
+            minHeap.offer(current);
+        }
+        System.out.println(sb.toString());
+    }
 }
+

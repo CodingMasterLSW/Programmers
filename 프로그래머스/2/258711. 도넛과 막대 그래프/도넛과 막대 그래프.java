bc440732c9b624ac185da5f51a065aec7e69f828
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-02-24 01:25:34 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 977.61 ms, Memory: 522 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,66 +2,110 @@ import java.util.*;
 
 class Solution {
     
-    Map<Integer, Node> nodes = new HashMap<>();
-    Set<Integer> arr = new HashSet<>();
+    static Map<Integer, Node> nodes = new HashMap<>();
+    static boolean[] visited;
     
     public int[] solution(int[][] edges) {
         init(edges);
-
-        Node king = findKing();
-        System.out.println(king.name);
-    
-        for (int key : nodes.keySet()) {
-            Node node = nodes.get(key);
-            node.removeKing(king.name);
-        } 
         
-        int stick = 0;
+        Node connectNode = findConnectNode();
+        removeConnectNodeIn(connectNode);
+        
         int donut = 0;
         int eight = 0;
+        int stick = 0;
         
+       
+        for (int i : connectNode.out) {
+            if (i == connectNode.name) {
+                continue;
+            }
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            int value = dfs(nodes.get(i));
+            
+            if (value == 1) {
+                eight++;
+            } else if (value == 2) {
+                donut++;
+            } else if (value == 3) {
+                stick++;
+            }
+        }
+
+        return new int[]{connectNode.name, donut, stick, eight};
+    }
+    
+    private int dfs(Node startNode) {
+        boolean hasDoubleOut = false;
+        Node current = startNode;
+        
+        while (true) {
+            if (current.out.isEmpty()) {
+                return 3;
+            }
+            
+            if (current.out.size() == 2) {
+                hasDoubleOut = true;
+            }
+            
+            int nextNode = current.out.get(0);
+            if (visited[nextNode]) {
+                break;
+            }
+            visited[nextNode] = true;
+            current = nodes.get(nextNode);
+        }
+        if (hasDoubleOut) {
+            return 1;
+        }
+        return 2;
+        
+    }
+    
+    private void removeConnectNodeIn(Node connectNode) {
         for (int key : nodes.keySet()) {
-            if (key == king.name) {
+            if (key == connectNode.name) {
                 continue;
             }
             Node node = nodes.get(key);
-            if (node.in.isEmpty()) {
-                stick++;
-            } else if (node.in.size() >= 2 && node.out.size() >= 2) {
-                eight++;
-            }
+            node.removeConnect(connectNode.name);
         }
-       
-        donut = king.out.size() - stick - eight;
-    
-        return new int[]{king.name, donut, stick, eight};
     }
     
-    public Node findKing() {
+    private Node findConnectNode() {
         for (int key : nodes.keySet()) {
             Node node = nodes.get(key);
-            if (node.in.isEmpty() && node.out.size() >=2) {
+            if (node.out.size() >= 2 && node.in.isEmpty()) {
                 return node;
             }
         }
         return null;
     }
     
-    public void init(int[][] edges) {
+    private void init(int[][] edges) {
+        Set<Integer> values = new HashSet<>();
+        
         for (int[] edge : edges) {
-            arr.add(edge[0]);
-            arr.add(edge[1]);
+            values.add(edge[0]);
+            values.add(edge[1]);
         }
-        for (int a : arr) {
-            nodes.put(a, new Node(a));
-        }
+        
+        int maxNode = Collections.max(values);
+        visited = new boolean[maxNode + 1];
+        
+        for (int v :values) {
+            nodes.put(v, new Node(v));
+        }      
         
         for (int[] edge : edges) {
             Node target = nodes.get(edge[0]);
-            Node conn = nodes.get(edge[1]);
+            Node connect = nodes.get(edge[1]);
             
-            target.out.add(conn.name);
-            conn.in.add(target.name);
+            target.out.add(connect.name);
+            connect.in.add(target.name);
         }
     }
 }
@@ -75,9 +119,9 @@ class Node {
         this.name = name;
     }
     
-    public void removeKing(int king) {
-        if (in.contains(king)) {
-            in.remove(Integer.valueOf(king));
+    public void removeConnect(int target) {
+        if (in.contains(target)) {
+            in.remove(new Integer(target));
         }
     }
 }

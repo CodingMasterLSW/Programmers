8ff38b2bf57ee2d52621d67ff0f6048510c5ad61
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-03-04 13:18:37 +0900

    [Silver II] Title: 상자넣기, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -4,11 +4,13 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[N+1];
         int[] dp = new int[N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i=1; i<=N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -17,21 +19,18 @@ class Main {
         dp[1] = 1;
 
         for (int i=2; i<=N; i++) {
-            int cnt = 0;
-
             for (int j=0; j<i; j++) {
                 if (arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
-
+        
         int maxValue = 0;
 
         for (int d : dp) {
-            maxValue = Math.max(d, maxValue);
+            maxValue = Math.max(maxValue, d);
         }
         System.out.println(maxValue);
-        
     }
 }
